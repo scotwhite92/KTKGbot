55834ba2af9d4311efcd14ece9dc7723511e6b57
scotwhite92 <scotwhite92@gmail.com>
2023-01-26 20:17:41 -0600

    improvement

--- ManaFallBot.py
+++ ManaFallBot.py
@@ -15,7 +15,7 @@ while True:
         pydirectinput.press('f1')
         time.sleep(.3)
         pydirectinput.press('f4')
-        time.sleep(5)
+        time.sleep(1)
         dungeonCounter += 1
         if dungeonCounter == 1:
             print("Cleared", dungeonCounter, "run")
@@ -26,37 +26,44 @@ while True:
     if PetSkill[0] != -1:
         pydirectinput.press('6')
         pydirectinput.press('6')
-        #time.sleep(.3)
+        time.sleep(.3)
         pydirectinput.press('f3')
-        #time.sleep(.3)
-        pydirectinput.keyDown('w')
-        time.sleep(.6)
-        pydirectinput.keyDown('v')
-        time.sleep(3)   
-        pydirectinput.keyUp('w')
-        time.sleep(1) 
+        time.sleep(.1)
+        pydirectinput.keyDown('z')
+        time.sleep(1)
+        pydirectinput.press('v')
+        pydirectinput.keyUp('z')
+        time.sleep(.5)
+        pydirectinput.press('3')
+        time.sleep(1)   
+
+    BlackScreen = imagesearch("./Images/BlackScreen.png")
+    if BlackScreen[0] != -1:
+        time.sleep(.5)
+    
+    LoadingScreen = imagesearch("./Images/LoadingScreen.png")
+    if LoadingScreen[0] != -1:
+        time.sleep(.5)
              
     BossBar = imagesearch("./Images/BossBar.png", precision=0.9)
     if BossBar[0] != -1:
-        print('boss found')
-        pydirectinput.keyDown('w')
-        time.sleep(1)
+        #print('boss found')
+        pydirectinput.press('z')
+        time.sleep(.5)
         pydirectinput.press('esc')
-        time.sleep(.3)
-        pydirectinput.press('shift')
-        time.sleep(.3)
-        pydirectinput.keyDown('y')
-        time.sleep(3)
-        pydirectinput.keyUp('y')
+        time.sleep(.2)
         pydirectinput.keyUp('w')
-        time.sleep(3)
+        pydirectinput.press('f2')
+        time.sleep(.3)
+        pydirectinput.press('y')
+        time.sleep(2)
         counter = 0
         while True:
             
             SkipMVP = imagesearch("./Images/Skip.png")
             if SkipMVP[0] != -1:
                 pydirectinput.press('esc')
-                time.sleep(5)
+                time.sleep(1)
                 break
             else:
                 counter +=1
@@ -89,8 +96,7 @@ while True:
                             pydirectinput.click(958,620)
                             time.sleep(1)
                             pydirectinput.press('f')
-                            time.sleep(10)
-                            pydirectinput.press('f4')
+                            time.sleep(1)
                             break
                         else:
                             print('Cannot find dungeon. Exiting...')
@@ -98,58 +104,3 @@ while True:
                     else:
                         print('Cannot head to town. Exiting...')
                         exit()  
-    else:
-        print('No boss found. Moving...')
-        counter = 0
-        while True:
-            pydirectinput.keyDown('w')
-            time.sleep(1)
-            pydirectinput.keyUp('w')
-            time.sleep(1)
-            BossBar = imagesearch("./Images/BossBar.png", precision=0.9)
-            if BossBar[0] != -1:
-                print('Boss found.')
-                counter = 0
-                break
-            counter +=1
-            #counter +=5
-
-            if counter == 3:
-                counter = 0
-                pydirectinput.press('enter')
-                time.sleep(.25)
-                pydirectinput.press('enter')
-                time.sleep(.25)
-                pydirectinput.press('enter')
-                time.sleep(.25)
-                pydirectinput.press('esc')
-                time.sleep(.5)
-                pydirectinput.press('esc')
-                time.sleep(2)
-                TownButton = imagesearch("./Images/EscMenu.png")
-                if TownButton[0] != -1:
-                    pydirectinput.click(1413,447)
-                    time.sleep(1)
-                    pydirectinput.press('enter')
-                    time.sleep(15)
-                    pydirectinput.keyDown('s')
-                    time.sleep(2)
-                    pydirectinput.keyUp('s')
-                    ConquestDungeon = imagesearch("./Images/SunShrineDungeon.png")
-                    if ConquestDungeon[0] != -1:
-                        pydirectinput.click(958,620)
-                        time.sleep(1)
-                        pydirectinput.press('f')
-                        time.sleep(10)
-                        pydirectinput.press('f4')
-                        break
-                    else:
-                        print('Cannot find dungeon. Exiting...')
-                        exit()
-                else:
-                    print('Cannot head to town. Exiting...')
-                    exit()  
-
-
-
-
